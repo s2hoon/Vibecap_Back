36736ce6e25af29ed71a91b29078b4d47dcc08ed
Mingeun Park <mindal99546@gmail.com>
2023-01-07 14:33:28 +0900

    feat(Encryptor): 비밀번호 암호화 기능 구현

---
+++ src/main/java/com/example/vibecap_back/util/Encryptor.java
@@ -0,0 +1,33 @@
+package com.example.vibecap_back.util;
+
+import com.example.vibecap_back.global.config.security.Secret;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Encryptor {
+
+    public String encrypt(String pwd) {
+        String result ="";
+
+        try{
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+            // pwd와 salt를 합친 문자열에 SHA-256 적용
+            md.update((pwd+ Secret.SALT).getBytes());
+            byte[] pwdsalt = md.digest();
+
+            // byte를 10진수 문자열로 변경
+            StringBuffer sb = new StringBuffer();
+            for (byte b: pwdsalt)
+                sb.append(String.format("%02x", b));
+
+            result = sb.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/com/example/vibecap_back/util/EncryptorTest.java
@@ -0,0 +1,20 @@
+package com.example.vibecap_back.util;
+
+import com.example.vibecap_back.util.Encryptor;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class EncryptorTest {
+
+    @Test
+    void salt_암호화_테스트() {
+        // given
+        String userPwd = "qwer1234";
+        String encryptedPwd;
+        Encryptor encryptor = new Encryptor();
+        // when
+        encryptedPwd = encryptor.encrypt(userPwd);
+        // then
+        Assertions.assertThat(encryptedPwd).isEqualTo(encryptor.encrypt("qwer1234"));
+    }
+}
