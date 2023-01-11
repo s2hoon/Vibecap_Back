8d4a4b5a88dac6f3ef5e923bb275705f3a193bce
Mingeun Park <mindal99546@gmail.com>
2023-01-11 17:04:22 +0900

    test: test db와 연결되었는지 확인하는 테스트

--- src/test/java/com/example/vibecap_back/domain/member/MemberRepositoryTest.java
+++ src/test/java/com/example/vibecap_back/domain/member/MemberRepositoryTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.stream.IntStream;
@@ -21,45 +22,27 @@ import java.util.stream.IntStream;
 @ActiveProfiles("test")
 public class MemberRepositoryTest {
 
-    private String TEST_EMAIL = "hello@naver.com";
-    private String TEST_PW = "qwer1234";
-    private String TEST_NICKNAME = "Nick";
+    private final String TEST_EMAIL = "hello@naver.com";
+    private final String TEST_PW = "qwer1234";
+    private final String TEST_NICKNAME = "Nick";
 
     @Autowired
     private MemberRepository memberRepository;
-    private MemberDto memberDto;
-    private Member member;
 
-    @BeforeEach
-    void init() {
-        memberDto = MemberDto.builder()
-                .email(TEST_EMAIL)
-                .password(TEST_PW)
-                .role(Authority.NORMAL)
-                .nickname(TEST_NICKNAME)
-                .status(MemberStatus.ACTIVE)
-                .build();
-
-        member = Member.builder()
-                .email(TEST_EMAIL)
-                .password(TEST_PW)
-                .role(Authority.NORMAL)
-                .nickname(TEST_NICKNAME)
-                .status(MemberStatus.ACTIVE)
-                .build();
-    }
-
+    /**
+     * DB와의 연결이 제대로 되었는지 확인하기 위해 dummy data를 삽입하고 조회.
+     */
     @Test
     @DisplayName("DB 동작 확인")
     void insertDummies() {
         IntStream.rangeClosed(1, 100).forEach(i->{
-            MemberDto dummy = createMemberDto(i);
-            memberRepository.save(member);
+            Member dummy = createMember(i);
+            memberRepository.save(dummy);
         });
     }
 
-    MemberDto createMemberDto(Integer i) {
-        MemberDto dummy = MemberDto.builder()
+    Member createMember(Integer i) {
+        Member dummy = Member.builder()
                 .email(TEST_EMAIL + i.toString())
                 .password(TEST_PW)
                 .role(Authority.NORMAL)
