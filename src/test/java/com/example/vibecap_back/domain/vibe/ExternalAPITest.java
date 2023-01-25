19890ad93ab117b7af3c4d8335dc132ab92b9acb
Mingeun Park <mindal99546@gmail.com>
2023-01-25 09:56:20 +0900

    test(vibe): 사진으로 검색하는 테스트 추가

--- src/test/java/com/example/vibecap_back/domain/vibe/ExternalAPITest.java
+++ src/test/java/com/example/vibecap_back/domain/vibe/ExternalAPITest.java
@@ -1,13 +1,21 @@
 package com.example.vibecap_back.domain.vibe;
 
+import com.example.vibecap_back.domain.member.dao.MemberRepository;
 import com.example.vibecap_back.domain.vibe.application.ImageAnalyzer;
 import com.example.vibecap_back.domain.vibe.application.Impl.LabelDetectionClient;
 import com.example.vibecap_back.domain.vibe.application.Impl.YouTubeClient;
 import com.example.vibecap_back.domain.vibe.application.PlaylistSearchEngine;
+import com.example.vibecap_back.domain.vibe.application.QueryMaker;
+import com.example.vibecap_back.domain.vibe.application.VibeService;
+import com.example.vibecap_back.domain.vibe.dao.VibeRepository;
+import com.example.vibecap_back.domain.vibe.exception.ExternalApiException;
 import com.example.vibecap_back.util.FileWorker;
+import com.google.api.services.youtube.model.Playlist;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
 /**
  * Google vision api, Youtube data api 호출 테스트
@@ -22,7 +30,8 @@ public class ExternalAPITest {
     // private static final String SAMPLE_IMAGE = "attack-on-titan.jpeg";
     // private static final String SAMPLE_IMAGE = "pokemon.jpeg";
     // private static final String SAMPLE_IMAGE = "flags.jpeg";
-     private static final String SAMPLE_IMAGE = "food.jpeg";
+    // private static final String SAMPLE_IMAGE = "food.jpeg";
+     private static final String SAMPLE_IMAGE = "sea.jpeg";
 
     public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_RESET = "\u001B[0m";
@@ -53,10 +62,31 @@ public class ExternalAPITest {
     }
 
     @Test
-    void 플레이리스트_추천() {
+    void 플레이리스트_추천_추가정보_이용() {
         Assertions.assertDoesNotThrow(() -> {
             System.out.println("www.youtube.com/watch?v=" +
-                    playlistSearchEngine.search("exciting morning"));
+                    playlistSearchEngine.search("눈 내리는 아침"));
         });
     }
+
+    @Test
+    void 플레이리스트_추천_사진_이용() {
+        ImageAnalyzer imageAnalyzer = new LabelDetectionClient();
+        PlaylistSearchEngine playlistSearchEngine = new YouTubeClient();
+        String label;
+        String videoId;
+
+        try {
+            label = imageAnalyzer.detectLabelsByWebReference(data);
+            videoId = playlistSearchEngine.search(label);
+            printFullURL(videoId);
+        } catch (ExternalApiException e) {
+            Assertions.fail(e.getMessage());
+        }
+    }
+
+    void printFullURL(String videoId) {
+        System.out.println("https://www.youtube.com/watch?v=" + videoId);
+    }
+
 }
