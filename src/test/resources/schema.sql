28f2e803049f6cde50452c5d4eac6ed2e309e191
Mingeun Park <mindal99546@gmail.com>
2023-01-29 15:07:59 +0900

    refactor(test): 테이블 스키마 업데이트

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -6,26 +6,16 @@ CREATE TABLE comment(
                         PRIMARY KEY (comment_id)
 );
 
-CREATE TABLE hashtag(
-                        member_id   INT     NOT NULL,
-                        tag_id      INT     NOT NULL,
+CREATE TABLE post_like(
+                          like_id     INT     NOT NULL AUTO_INCREMENT,
+                          member_id   INT     NOT NULL,
+                          post_id     INT     NOT NULL,
 
-                        PRIMARY KEY (member_id, tag_id),
-                        FOREIGN KEY (member_id) REFERENCES member(member_id)
-                            ON DELETE CASCADE,
-                        FOREIGN KEY (tag_id) REFERENCES tag(tag_id)
-                            ON DELETE CASCADE
-);
-
-CREATE TABLE _like(
-                      member_id   INT     NOT NULL,
-                      post_id     INT     NOT NULL,
-
-                      PRIMARY KEY (member_id, post_id),
-                      FOREIGN KEY (member_id) REFERENCES member(member_id)
-                          ON DELETE CASCADE,
-                      FOREIGN KEY (post_id) REFERENCES post(post_id)
-                          ON DELETE CASCADE
+                          PRIMARY KEY (like_id),
+                          FOREIGN KEY (member_id) REFERENCES member(member_id)
+                              ON DELETE CASCADE,
+                          FOREIGN KEY (post_id) REFERENCES post(post_id)
+                              ON DELETE CASCADE
 );
 
 CREATE TABLE member(
@@ -36,7 +26,7 @@ CREATE TABLE member(
                        role            CHAR(16)    NOT NULL,
                        nickname        CHAR(64)    NOT NULL,
                        state           CHAR(16)    NOT NULL,
-                       profile_image   MEDIUMBLOB  NOT NULL,
+                       profile_image   CHAR(512) ,
 
                        PRIMARY KEY (member_id)
 );
@@ -44,27 +34,41 @@ CREATE TABLE member(
 CREATE TABLE post(
                      post_id         INT         NOT NULL AUTO_INCREMENT,
                      member_id       INT         NOT NULL ,
-                     title           CHAR(32)    NOT NULL ,
-                     body            TEXT        NOT NULL ,
+                     post_title           CHAR(32)    NOT NULL ,
+                     post_body            TEXT        NOT NULL ,
                      vibe_id         INT         NOT NULL ,
                      like_number     INT         DEFAULT 0,
                      scrap_number    INT         DEFAULT 0,
-                     tag_number      INT         DEFAULT 0,
+                     comment_number      INT         DEFAULT 0,
 
                      PRIMARY KEY (post_id),
                      FOREIGN KEY (member_id) REFERENCES member(member_id)
                          ON DELETE CASCADE
 );
 
-CREATE TABLE scrap(
-                      member_id   INT     NOT NULL,
-                      post_id     INT     NOT NULL,
+CREATE TABLE post_scrap(
+                           scrap_id    INT     NOT NULL AUTO_INCREMENT,
+                           member_id   INT     NOT NULL,
+                           post_id     INT     NOT NULL,
 
-                      PRIMARY KEY (member_id, post_id),
-                      FOREIGN KEY (member_id) REFERENCES member(member_id)
-                          ON DELETE CASCADE,
-                      FOREIGN KEY (post_id) REFERENCES post(post_id)
-                          ON DELETE CASCADE
+                           PRIMARY KEY (scrap_id),
+                           FOREIGN KEY (member_id) REFERENCES member(member_id)
+                               ON DELETE CASCADE,
+                           FOREIGN KEY (post_id) REFERENCES post(post_id)
+                               ON DELETE CASCADE
+);
+
+CREATE TABLE sub_comment
+(
+    sub_comment_id   INT       NOT NULL AUTO_INCREMENT,
+    comment_id       INT       NOT NULL,
+    sub_comment_body CHAR(255) NOT NULL DEFAULT 'empty comment',
+    member_id        INT       NOT NULL,
+    post_id          INT       NOT NULL,
+
+    PRIMARY KEY (sub_comment_id),
+    FOREIGN KEY (member_id) REFERENCES member(member_id),
+    FOREIGN KEY (post_id) REFERENCES post(post_id)
 );
 
 CREATE TABLE tag (
@@ -77,7 +81,7 @@ CREATE TABLE tag (
 CREATE TABLE vibe(
                      vibe_id         INT         NOT NULL AUTO_INCREMENT,
                      member_id       INT         NOT NULL,
-                     vibe_image      MEDIUMBLOB  NOT NULL,
+                     vibe_image      CHAR(512)  NOT NULL,
                      youtube_link    CHAR(255)   NOT NULL,
                      vibe_keywords   CHAR(255)   NOT NULL,
 
