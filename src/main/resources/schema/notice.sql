ee1253b495b5ee870e4fdb5bad2b45aa76ef5a95
Mingeun Park <mindal99546@gmail.com>
2023-01-30 17:44:04 +0900

    sql(notice): notice 테이블 스키마 추가

--- src/main/resources/schema/notice.sql
+++ src/main/resources/schema/notice.sql
@@ -1,10 +1,13 @@
 CREATE TABLE notice(
-    type            INT         NOT NULL ,
-    payload_id      INT         NOT NULL ,
-    created_time    DATETIME(6) NOT NULL ,
-    receiver_id     INT         NOT NULL ,
+    notice_id               INT             NOT NULL AUTO_INCREMENT ,
+    target_type             INT             NOT NULL ,
+    target_id               INT             NOT NULL ,
+    event_type              INT             NOT NULL ,
+    created_time            DATETIME(6)     NOT NULL ,
+    receiver_id             INT             NOT NULL ,
+    is_read                 BOOLEAN         DEFAULT FALSE ,
 
-    PRIMARY KEY (type, payload_id),
+    PRIMARY KEY (notice_id) ,
     FOREIGN KEY (receiver_id) REFERENCES member(member_id)
                    ON DELETE CASCADE
 );
