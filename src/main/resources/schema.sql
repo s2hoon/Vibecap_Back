c5621ae54e02b1a493e0a428de3848028cffdfea
Mingeun Park <mindal99546@gmail.com>
2023-01-30 16:09:40 +0900

    fix: 오타 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE comment(
                         comment_id          INT         NOT NULL AUTO_INCREMENT,
                         post_id             INT         NOT NULL,
-                        commend_body        CHAR(255)   NOT NULL,
+                        comment_body        CHAR(255)   NOT NULL,
 
                         PRIMARY KEY (comment_id)
 );
